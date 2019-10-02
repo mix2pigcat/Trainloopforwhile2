6e4b47d0d691205a6cd22e3214aa90b269c0cd6a
thanhle <mix2pigcat@gmaail.com>
2019-10-02 17:41:10 +0700

    filebai2

---
+++ Javascript.js
@@ -0,0 +1,13 @@
+let a=parseInt(prompt('Nhap so do :'));
+while (a>100||a<20){
+    if (a>100){
+        alert('Giam nhiet do');
+        a=parseInt(prompt('Nhap so do :'));
+        }else if(a<20){
+        alert('Tang nhiet do');
+        a=parseInt(prompt('Nhap so do :'));
+    }
+    alert("nhap lai nhiet do");
+
+
+}
